47862a5daf95ce91c75065339e88ce4288e668f8
Nour <nourallazkani@gmail.com>
2016-02-13 10:25:29 +0100

    cleanup

--- src/main/java/org/sjr/babel/entity/Course.java
+++ src/main/java/org/sjr/babel/entity/Course.java
@@ -5,9 +5,7 @@ import java.util.Date;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Transient;
 @Entity //@Table(name="Course")
 public class Course extends AbstractEntity {

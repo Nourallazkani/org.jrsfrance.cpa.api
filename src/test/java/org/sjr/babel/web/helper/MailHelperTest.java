de03f08bc6c350f7ab55dd04da6cb0c33b2439d6
Alaric <alaric_hermant@hotmail.com>
2016-12-14 11:05:54 +0100

    mail templates simplification

---
+++ src/test/java/org/sjr/babel/web/helper/MailHelperTest.java
@@ -0,0 +1,30 @@
+package org.sjr.babel.web.helper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sjr.babel.JpaConfig4Tests;
+import org.sjr.babel.WebAppInitializer.RestConfiguration;
+import org.sjr.babel.web.helper.MailHelper.MailType;
+import org.sjr.babel.web.helper.MailHelper.SendMailResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {RestConfiguration.class, JpaConfig4Tests.class})
+public class MailHelperTest {
+
+	@Autowired
+	private MailHelper helper;
+	
+	@Test
+	public void testSend() {
+		SendMailResponse resp = helper.send(MailType.REFUGEE_SIGN_UP_CONFIRMATION, "fr", "a@x.fr", "x", "y");
+		assertEquals("Bienvenue sur le site CPA", resp.subject);
+	}
+
+}
